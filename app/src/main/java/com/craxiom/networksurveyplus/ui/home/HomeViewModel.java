3792bb18328881d550d8f5fe567fc098865145ce
christian.rowlands <christian.rowlands@gmail.com>
2020-07-08 16:11:30 -0500

    Initial commit of an template android app

---
+++ app/src/main/java/com/craxiom/networksurveyplus/ui/home/HomeViewModel.java
@@ -0,0 +1,22 @@
+package com.craxiom.networksurveyplus.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel
+{
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel()
+    {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText()
+    {
+        return mText;
+    }
+}
