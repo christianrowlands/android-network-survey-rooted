354f997e408a03672ed6179db16508bc27c4446f
christian.rowlands <christian.rowlands@gmail.com>
2020-09-01 13:29:31 -0400

    Added the same test to the androidTest

--- app/src/androidTest/java/com/craxiom/networksurveyplus/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/craxiom/networksurveyplus/ExampleInstrumentedTest.java
@@ -5,6 +5,11 @@ import android.content.Context;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.craxiom.networksurveyplus.messages.DiagRevealerMessageHeaderPreon;
+
+import org.codehaus.preon.Codec;
+import org.codehaus.preon.Codecs;
+import org.codehaus.preon.DecodingException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -24,5 +29,27 @@ public class ExampleInstrumentedTest
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.craxiom.networksurveyplus", appContext.getPackageName());
+    }
+
+    @Test
+    public void testPreonParsingForDiagHeader()
+    {
+        final byte[] headerBytes = {1, 0, 13, 9};
+        final short expectedMessageType = 1;
+        final short expectedMessageLength = 2317;
+
+        final Codec<DiagRevealerMessageHeaderPreon> headerCodec = Codecs.create(DiagRevealerMessageHeaderPreon.class);
+        final DiagRevealerMessageHeaderPreon headerPreon;
+        try
+        {
+            headerPreon = Codecs.decode(headerCodec, headerBytes);
+        } catch (DecodingException e)
+        {
+            fail("Could not decode the diag revealer header bytes");
+            return;
+        }
+
+        assertEquals(expectedMessageType, headerPreon.messageType);
+        assertEquals(expectedMessageLength, headerPreon.messageLength);
     }
 }
